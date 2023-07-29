51802dfedc80452045241c2567d9c3ffba4c7bb1
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-29 21:50:07 +0100

    Add the deploy script for Faucet.sol

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const faucet = await hre.ethers.deployContract("Faucet");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await faucet.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Faucet deployed to ${faucet.target}`
   );
 }
 
